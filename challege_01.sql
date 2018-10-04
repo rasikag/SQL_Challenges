a874b0633ebcda0a4b035bf59d0a89b63c4c2a7b
Koggala Mudiyanselage, Rasika Gayan Gunarathna <rasikag@hp.com>
2018-10-04 18:53:36 +0800

    challenge 01

---
+++ challege_01.sql
@@ -0,0 +1,28 @@
+
+-- This query filters orders that were placed by customer 71; groups those orders by employee and
+-- order year; and filters only groups of employees and years that have more than one order. For the
+-- remaining groups, the query presents the employee ID, order year, and count of orders, sorted by the
+-- employee ID and order year. For now, don’t worry about understanding how this query does what it
+-- does; I’ll explain the query clauses one at a time, and gradually build this query.
+
+use TSQLV4
+
+
+SELECT empid, 
+YEAR(orderdate) AS orderyear, 
+COUNT(*) AS numorders
+FROM Sales.Orders
+WHERE custid = 71
+GROUP BY empid, YEAR(orderdate)
+HAVING COUNT(*) > 1
+ORDER BY empid, orderyear
+
+
+
+
+
+
+
+
+
+
